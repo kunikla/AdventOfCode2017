fef65f5ab9f4c4fae6b307090ec2e7b98e6b9019
Christina Hamilton <kunikla.vt@gmail.com>
2017-12-25 18:21:01 -0500

    Version 1.0

---
+++ js/d1p1.js
@@ -0,0 +1,28 @@
+function match(char1, char2) {
+  if (char1 != char2) return 0;
+  var result = parseInt(char1, 10);
+  return (isNaN(result) ? 0 : result);
+}
+
+function captcha(input) {
+  var inputLength = input.length;
+  if (inputLength < 2) return 0;
+  var solution = 0;
+  var firstChar = input.slice(0, 1);
+  var curChar = "";
+  var nxtChar = firstChar;
+  for (i = 0 ; i < (inputLength-1); i++) {
+    curChar = nxtChar;
+    nxtChar = input.slice(i+1, i+2);
+    solution += match(curChar, nxtChar);
+  }
+  solution += match(nxtChar, firstChar);
+  return solution;
+}
+
+document.getElementById('btn').addEventListener('click', function (event) {
+  var aocString = document.getElementById("input-string").value;
+  console.log("aocString = " + aocString);
+  document.getElementById("solution-value").innerHTML = captcha(aocString);
+  console.log("Solution is " + document.getElementById("solution-value").innerHTML)
+});
